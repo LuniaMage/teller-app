0a15a9f9a609bb18956140e42da65f6ecb22a08d
LuniaMage <luniamage@gmail.com>
2020-05-11 15:47:57 -0400

    initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="about">
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      msg: "This is the About page",
+    }
+  }
+}
+</script>
+
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>{{msg}}</h1>
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Dashboard',  
+  data() {
+    return {
+      msg: "This is the Dashboard page",
+    }
+  }
+}
+</script>
